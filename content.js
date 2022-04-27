4909de1934adbbe916aef50036e144f9b0317924
Günther101 <alex@ts-soft.info>
2022-04-27 19:19:28 +0200

    feat: Add Slider | flexible compact redesign
    Changed display and alignment to get  more centered and flexible container when scaling, also to create more space for the slider, so it won´t displace the chapter container while you aren´t in fullscreen.
    
    Added two stacked slider, because I didn´t know a other way to increase the interactable area of the slider and change design while stay in html instyle, for faster and more precise pb-values.
    
    Co-Authored-By: Jan Klass <kissaki@posteo.de>

--- content.js
+++ content.js
@@ -1,23 +1,42 @@
 function createContainer() {
   let container = document.createElement('div')
   container.className = 'pbspeed-container'
-  container.style = 'margin:0 14px; display:grid; grid-template-columns: auto auto; gap:12px;'
+  container.style = 'margin:0 14px; display:grid; grid-template-columns: auto auto; gap:10px;'
   // Control layout:
   // | Display | 0.25 0.50 0.75 1.00
   // | Current | 1.25 1.50 1.75 2.00
-  container.innerHTML = `<div class="rdisplay" style="display:inline-block; font-size:22px;">⏱ <span class="pbspeed-value"></span></div>
-  <div class="setrs" style="display: grid; grid-template: 1fr 1fr / repeat(4, auto); column-gap: 6px;"><div>0.25</div><div>0.50</div><div>0.75</div><div>1.00</div><div>1.25</div><div>1.50</div><div>1.75</div><div>2.00</div></div>`
+
+  // little changes for compact design | added two slider(stacked): "input" for actual interaction and "fake" for visual reasons   
+  container.innerHTML = `<div class="rdisplay" style="display:flex; justify-content:center; align-items:center;">⏱ <output class="pbspeed-value" style="display:flex;margin: 0em 0.4em 0em 0.2em"></output><div class="sliderdiv" style="display:flex;height:100%;justify-content:center;align-items:center"> <input class="pbspeed-slider" id="sliderinput" type="range" min="0" max="2" step="0.05" style="z-index:1;cursor:pointer;width:7em;height:100%;opacity:0"></input> <input class="pbspeed-slider" type="range" id="sliderfake" min="0" max="2" step="0.05" style="position:absolute;z-index:-1;width:7em;height:0.72em;-webkit-appearance:none;outline:none;opacity:0.70;background:#111111;box-shadow: inset 0 0 5px rgba(0, 0, 0, 1);"/></div>
+  </div> 
+ 
+  <div class="setrs" style="display:grid; grid-template-columns:repeat(4, auto); gap: 2px; margin: 0.3em 0em 0.3em 0em"><div>0.25</div><div>0.50</div><div>0.75</div><div>1.00</div><div>1.25</div><div>1.50</div><div>1.75</div><div>2.00</div></div>`
 
   let vid = document.querySelector('video.html5-main-video')
   let valEl = container.querySelector('.pbspeed-value')
-  let updateDisplay = () => valEl.innerText = `${vid.playbackRate.toLocaleString(undefined, { minimumFractionDigits: 2 })}x`
+  // Var the operational slider
+  let slider = container.querySelector('#sliderinput')
+
+  let updateDisplay = function(){
+    valEl.innerText = `${vid.playbackRate.toLocaleString(undefined, { minimumFractionDigits: 2 })}x`;
+  // Update visible slider thumb position 
+    slider.value = vid.playbackRate;
+    container.querySelector('#sliderfake').value = slider.value
+  }
+
   updateDisplay()
   vid.addEventListener('ratechange', updateDisplay)
+ 
+  // Slider: mouseover selection to directly manipulate value with mousewheel | Pass value
+  slider.addEventListener('mouseover', () => slider.focus())
+  slider.addEventListener('input', () => vid.playbackRate = slider.value)
+
+
 
   container.querySelectorAll('.setrs > *').forEach(x => {
-    x.addEventListener('click', () => vid.playbackRate = x.innerText)
+    x.addEventListener('click', () => vid.playbackRate = parseFloat(x.innerText).toFixed(2))
     // container height 48px => element height 48 / 2 = 24 px
-    x.style = 'font-size: 14px; line-height: 24px; display: flex; align-items: center; cursor: pointer;'
+    x.style = 'font-size: 12px; line-height: normal; display: flex; align-items: center; cursor: pointer; padding: 0px 2px;'
   })
 
   return container
